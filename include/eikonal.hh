084bc776c8905de311d0f7097ac2ec8a67bbfeac
snatesh <sachin.natesh@gmail.com>
2024-10-01 07:37:03 -0600

    add eikonal solver lib

---
+++ include/eikonal.hh
@@ -0,0 +1,127 @@
+#ifndef _EIKONAL_H
+#define _EIKONAL_H
+
+#include<cblas.h>
+#include<nlopt.h>
+#include<cstdlib>
+#include<iostream>
+#include<cstring>
+struct optData
+{
+
+  unsigned int N;
+  double *vecx, *vecy, *vecxx, *vecyy;
+  double *vecf, *vecrhs;
+  double *Dx, *Dy, *Cf;
+  double *vl, *vb, *vh;
+  double* cu;
+ 
+  optData ( unsigned int _N,
+            double* _Dx, double* _Dy,
+            double* _vl, double* _vb, 
+            double* _vh, double* _cu ) 
+    : N(_N), Dx(_Dx), Dy(_Dy), 
+      vl(_vl), vb(_vb), vh(_vh),
+      cu(_cu)
+  {
+    // Coeff derivs in x,y
+    vecx = (double*) calloc(N, sizeof(double));
+    vecy = (double*) calloc(N, sizeof(double));
+    // outer product of coef derivs
+    vecxx = (double*) calloc(N*N, sizeof(double));
+    vecyy = (double*) calloc(N*N, sizeof(double));
+    // objective function 
+    // (combination of outer products, vectorized)
+    vecf = (double*) calloc(N*N, sizeof(double));
+    // outer product of coeffs of (1/f)
+    // first is entry is 1 the case of f \equiv 1
+    vecrhs = (double*) calloc(N*N, sizeof(double)); 
+    vecrhs[0] = 1; 
+  }
+
+  ~optData ()
+  {
+    free(vecx); free(vecy);
+    free(vecxx); free(vecyy);
+    free(vecf); free(vecrhs);
+  }
+
+};
+                
+static unsigned long count = 0;
+double F  ( unsigned int n, const double* cu, 
+            double* grad, void* _data  )
+{
+  ++count;
+  optData* data     = (optData*) _data;
+  unsigned int N    = data->N;
+  unsigned int NN   = N*N;
+  double* vecx      = data->vecx; 
+  double* vecy      = data->vecy;
+  double* vecxx     = data->vecxx; 
+  double* vecyy     = data->vecyy; 
+  double* Dx        = data->Dx;
+  double* Dy        = data->Dy;
+  double* vecf      = data->vecf;
+  double* vecrhs    = data->vecrhs;
+  memset(vecxx, 0, NN * sizeof(double));
+  memset(vecyy, 0, NN * sizeof(double));
+  cblas_dgemv ( CblasColMajor, CblasNoTrans,  
+                N, N, 1.0, Dx, N, cu, 1, 0.0, vecx, 1 ); 
+  cblas_dgemv ( CblasColMajor, CblasNoTrans,  
+                N, N, 1.0, Dy, N, cu, 1, 0.0, vecy, 1 ); 
+  cblas_dger  ( CblasColMajor, N, N, 
+                1.0, vecx, 1, vecx, 1, vecxx, N );
+  cblas_dger  ( CblasColMajor, N, N, 
+                1.0, vecy, 1, vecy, 1, vecyy, N );
+  #pragma omp simd
+  for (unsigned int i = 0; i < NN; ++i)
+  {
+    vecf[i] = vecxx[i] + vecyy[i] - vecrhs[i];
+  } 
+
+  // frobenius norm of A \equiv 2 norm vec(A)  
+  double nrm = cblas_dnrm2 (NN, vecf, 1);
+  double nrm2 = nrm * nrm;
+  if ( !(count % 100) ) 
+  { 
+    std::cout << "Eval #" << count << " : F = " << nrm2 << std::endl; 
+  }
+  return nrm2;
+}
+
+
+
+void cl ( unsigned int N, double* result, unsigned int n, 
+          const double* cu, double* grad, void* f_data)
+{
+  n = N;
+  optData* data = (optData*) f_data;
+  double* vl = data->vl;
+  cblas_dgemv ( CblasColMajor, CblasNoTrans,  
+                N, N, 1.0, vl, N, cu, 1, 0.0, result, 1 ); 
+}
+
+void cb ( unsigned int N, double* result, unsigned int n, 
+          const double* cu, double* grad, void* f_data)
+{ 
+  n = N;
+
+  optData* data = (optData*) f_data;
+  double* vb = data->vb;
+  cblas_dgemv ( CblasColMajor, CblasNoTrans,  
+                N, N, 1.0, vb , N, cu, 1, 0.0, result, 1 ); 
+}
+
+void ch ( unsigned int N, double* result, unsigned int n, 
+          const double* cu, double* grad, void* f_data)
+{
+  n = N;
+  optData* data = (optData*) f_data;
+  double* vh = data->vh;
+  cblas_dgemv ( CblasColMajor, CblasNoTrans,  
+                N, N, 1.0, vh , N, cu, 1, 0.0, result, 1 ); 
+}
+
+
+#endif
