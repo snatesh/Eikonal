20a356d76f4d1beed21a9dd1f9af130dc95948c8
snatesh <sachin.natesh@gmail.com>
2024-12-10 19:58:54 -0700

    derived limit relation for evaluating jacobi poly on triangle at x=1. implemented reduced objective in c. gradient of reduced objective remains.

---
+++ include/ngjquad_redobj.hh
@@ -0,0 +1,414 @@
+#ifndef _NGJQUAD_REDOBJ_H
+#define _NGJQUAD_REDOBJ_H
+
+#include<algorithm>
+#include<complex.h>
+#include<cblas.h>
+#include<lapacke.h>
+#include<nlopt.h>
+#include<jMat.hh>
+#include<dMat.hh>
+#include<jPoly.hh>
+#include<jevd.hh>
+
+typedef double _Complex Complex;
+static unsigned long count = 0;
+static unsigned long count1 = 0;
+
+
+struct optData
+{
+
+  double *Vm = 0;
+  double *Jn1 = 0, *Jn2 = 0;
+  Complex *Jnz = 0, *XY0 = 0;
+  double *X0 = 0, *Y0 = 0;
+  double *Q = 0, *W0 = 0;
+  double* one = 0;
+  jMat<double>* Jn = 0;
+  jPoly<double>* Pm = 0;
+  double a, b, c, d;
+  unsigned int N, M, n, m;
+  bool run0; 
+  double minf;
+  unsigned int nthreads;
+  unsigned int dim;
+  double* Z0 = 0; 
+  double* G = 0;
+  double* Habc = 0;
+
+  std::string dir;
+  std::string splxT;
+  
+  void init()
+  {
+  
+    initmsg(0);
+    if (this->dim == 2)
+    {
+      this->N = static_cast<unsigned int>(0.5 * n * (n + 1)); 
+      this->M = static_cast<unsigned int>(0.5 * m * (m + 1));
+      // generate jacobi matrices for x,y 
+      this->Jn = new jMat<double>(n, a, b, c);
+      this->Pm = new jPoly<double>(N, m-1, a, b, c, nthreads); 
+      this->Habc  = (double*) calloc((m+1)*(m+1), sizeof(double));
+      sFactors(m+1, a, b, c, Habc);  
+      this->Vm = Pm->V;
+      this->Jn1 = Jn->Jn1;
+      this->Jn2 = Jn->Jn2;
+      this->Jnz = (Complex*) calloc(N*N, sizeof(Complex));
+      this->XY0 = (Complex*) calloc(N, sizeof(Complex));
+      this->X0  = (double*) calloc(N, sizeof(double));
+      this->Y0  = (double*) calloc(N, sizeof(double));
+      this->Q   = (double*) calloc(N*N, sizeof(double));  
+      this->W0  = (double*) calloc(N, sizeof(double));
+      this->one = (double*) calloc(N, sizeof(double));
+      for (unsigned int i = 0; i < N; ++i) { one[i] = 1.0; }
+      this->Z0 = (double*) calloc(dim*N, sizeof(double));
+      this->G = (double*) calloc((dim+1)*N*(dim*N), sizeof(double));
+      for (unsigned int i = 0; i < dim*N; ++i)
+      {
+        G[i + i*((dim+1)*N)] = -1.0;
+      }   
+      for (unsigned int i = dim*N; i < (dim+1)*N; ++i)
+      {
+        for (unsigned int j = 0; j < dim; ++j)
+        {   
+          G[i + ((i-dim*N)+j*N)*((dim+1)*N)] = 1.0;
+        }   
+      }
+    }
+    else
+    {
+      std::cerr << "only dim=2 is supported. Exiting..\n";
+      exit(1);
+    }
+    initmsg(1);
+  }
+   
+  
+  optData ( unsigned int _m, unsigned int _n, 
+            double _a, double _b, double _c, 
+            unsigned int _nthreads )
+    : n(_n), m(_m), a(_a), b(_b), c(_c), 
+      run0(true), dim(2), nthreads(_nthreads) { this->init(); }
+  
+  ~optData()
+  {
+    if (Jn) { delete Jn; Jn = 0; }
+    if (Pm) { delete Pm; Pm = 0; }
+    if (X0) { free(X0); X0 = 0; }
+    if (Y0) { free(Y0); Y0 = 0; }
+    if (Z0) { free(Z0); Z0 = 0; }
+    if (Q) { free(Q); Q = 0; }
+    if (W0) { free(W0); W0 = 0; }
+    if (XY0) { free(XY0); XY0 = 0; }
+    if (Jnz) { free(Jnz); Jnz = 0; }
+    if (G)  { free(G); G = 0; }
+    if (Habc) { free(Habc); Habc = 0; }
+    if (one) { free(one); one = 0; }
+  }
+
+  void initmsg(unsigned int step)
+  {
+    if (this->dim < 1 || this->dim > 3)
+    {   
+      std::cerr << "dim must be 1,2 or 3! Exiting ..\n";
+      exit(1);
+    }
+    if (step == 0) 
+    {
+      splxT = (this->dim == 2 ? "(TRIANGLE)" : 
+                (this->dim == 3 ? "(TETRAHEDRON)" : "(LINE)"));
+      std::cout << "\nBEGIN NGJQUAD " << splxT << " INITIALIZATION\n"; 
+    }
+    else if (step == 1)
+    {
+      std::cout << "\nORDER OF SOURCE BASIS : " << n-1 << std::endl;
+      std::cout << "ORDER OF TARGET BASIS : " << m-1 << std::endl;
+      std::cout << "\nSEARCHING FOR QUADRATURE RULE OF SIZE N = " << N << std::endl;
+      std::cout << "TO EXACTLY INTEGRATE M = " << M << " POLYNOMIALS\n"
+                << "WITH TOTAL DEGREE m = " << 0 << " .. " << m - 1 << "\n\n";
+      std::cout << "END NGJQUAD " << splxT << " INITIALIZATION\n";
+    }
+  }
+
+};
+
+
+struct ngjQuad
+{
+  optData* optdata = 0;
+  unsigned int n, m;
+  double tol, tolc, alph;
+  bool use_newton, use_wolfe;
+  unsigned int nthreads;
+  nlopt_algorithm alg;
+  unsigned int dim;
+ 
+  ngjQuad ( unsigned int _n, unsigned int _m, 
+            double a, double b, double c, 
+            double _tol, double _tolc, double _alph,
+            bool _use_newton, bool _use_wolfe,
+            nlopt_algorithm _alg, unsigned int _nthreads)
+    : n(_n), m(_m), tol(_tol), tolc(_tolc), alph(_alph),
+      use_newton(_use_newton), use_wolfe(_use_wolfe),
+      alg(_alg), nthreads(_nthreads), dim(2)
+  {
+    this->optdata = new optData(m, n, a, b, c, nthreads);
+  }
+  
+  ~ngjQuad() 
+  { 
+    count = count1 = 0;
+    if (optdata) { delete optdata; optdata = 0; }
+  }
+  
+  
+  static inline void cond ( double* Vm, unsigned int N, 
+                            unsigned int M, double* rcond, 
+                            bool norm2=true )
+  {
+    // 2-norm cond
+    if (norm2)
+    {
+      unsigned int dimS = (N <= M) ? N : M;
+      double* S = (double*) calloc(dimS, sizeof(double));
+      double* superb = (double*) calloc(dimS, sizeof(double));
+  
+      LAPACKE_dgesvd( LAPACK_COL_MAJOR, 'N','N', N, M, Vm, N, S, 
+                      nullptr, 1, nullptr, 1, superb  );
+      rcond[0] = S[dimS-1] / S[0];
+  
+      free(S); free(superb);
+    }
+    // inf norm cond
+    else
+    {
+      int* ipiv = (int*) calloc(N, sizeof(int));
+      double normVm = LAPACKE_dlange(LAPACK_COL_MAJOR, '1', N, M, Vm, N);
+      // LU of A
+      LAPACKE_dgetrf(LAPACK_COL_MAJOR, N, M, Vm, N, ipiv); 
+      LAPACKE_dgecon(LAPACK_COL_MAJOR, '1', N, Vm, M, normVm, rcond);
+      free(ipiv);
+    }
+  }
+ 
+  static inline double optF_help (  unsigned int n, const double* Zk, 
+                                    double* grad, void* _data  )
+  {
+    ++count;
+    optData* data = (optData*) _data;
+    unsigned int m = data->m;
+    unsigned int M = data->M;
+    unsigned int N = data->N;
+    unsigned int dim = data->dim;
+    double *Vm = data->Pm->V;
+    const double *Xk, *Yk;
+    if (dim == 2)
+    {
+      Xk = Zk; Yk = Zk + N; 
+      data->Pm->computeV(Xk, Yk);
+      // compute V*V'
+      cblas_dgemm ( CblasColMajor, 
+                    CblasNoTrans, 
+                    CblasTrans, 
+                    N, N, M, 
+                    1.0, Vm, N, 
+                    Vm, N,  
+                    0.0, data->Q, N );
+      //printMat(data->Vm, N, M); 
+      // perform cholesky Q = U'*U
+      // with upper triangular U stored
+      // in upper triangle block of Q
+      int info = LAPACKE_dpotrf (LAPACK_COL_MAJOR, 'U', N, data->Q, N);
+      if ( !info )
+      {
+        // if cholesky successfull, use factors to compute Q\1
+        LAPACKE_dpotrs  ( LAPACK_COL_MAJOR, 'U', N, 1,
+                          data->Q, N, data->one, N);
+      }
+      else
+      {
+        std::cerr << "Error: Cholesky factorization of Q. \n";
+        std::cerr << "Info code: " << info << std::endl;
+        std::cerr << data->Q << std::endl;
+        exit(1);
+      }
+      // copy sol to proper location, reset rhs
+      for (unsigned int i = 0; i < N; ++i)
+      {
+        data->W0[i] = data->one[i];
+        data->one[i] = 1.0;
+      }
+      // compute 1*W (sum of weights)
+      double sum = 0.0;
+      #pragma omp simd reduction(+:sum)
+      for (unsigned int i = 0; i < N; ++i) { sum += data->W0[i]; }
+      double fval = -0.5 * (sum - 1.0);     
+      if ( !(count % 1000) && data->run0 ) 
+      { 
+        std::cout << "Eval #" << count << " : F = " << fval << std::endl; 
+      }
+ 
+      return fval;
+    
+    }
+    else
+    {
+      std::cerr << "Only dim=2 is supported. Exiting..\n";
+      exit(1);
+    }
+  
+  }
+ 
+  static inline double optF ( unsigned int n, const double* Zk,
+                              double* grad, void* _data )
+  {
+    optData* data = (optData*) _data;
+    return optF_help(n, Zk, grad, _data);
+  }
+  
+  static inline void optieqC (  unsigned int m, double* result, unsigned int n, 
+                                const double* Zk, double* grad, 
+                                void* _data )
+  {
+    optData* data = (optData*) _data;
+    unsigned int dim = data->dim;
+    if (dim == 2)
+    {
+      unsigned int N = data->N;
+      #pragma omp simd
+      for (unsigned int i = 0; i < N; ++i)
+      {
+        result[i] = Zk[i] + Zk[i + N] - 1.0 + 1e-8; 
+      }
+    }
+  }
+  
+  inline void init ()
+  {
+    unsigned int m = optdata->m; unsigned int n = optdata->n; 
+    unsigned int M = optdata->M; unsigned int N = optdata->N;
+    double* Vm = optdata->Vm; 
+    double* Z0 = optdata->Z0; 
+    if (this->dim == 2)
+    {
+      double* Jn1 = optdata->Jn1; double* Jn2 = optdata->Jn2;
+      Complex* XY0 = optdata->XY0;
+      // compute initial nodes and weights from eigenvalues of Jn
+      for (unsigned int i = 0; i < N*N; ++i) { optdata->Jnz[i] = Jn1[i] + I*Jn2[i]; }
+  
+      if (LAPACKE_zgeev ( LAPACK_COL_MAJOR, 'N', 'N', N, optdata->Jnz, N, 
+                          XY0, nullptr, N, nullptr, N ))
+      {
+        std::cerr << "ERROR: NGJQUAD INIT" << std::endl;
+      }
+  
+      for (unsigned int i = 0; i < N; ++i) 
+      {
+        optdata->X0[i] = creal(XY0[i]);
+        optdata->Y0[i] = cimag(XY0[i]);
+      }
+      // evaluate Vandermonde on initial nodes
+      optdata->Pm->computeV(optdata->X0, optdata->Y0);
+    }
+    else
+    {
+      std::cerr << "Only dim=2 is supported. Exiting..\n";
+      exit(1);
+    }
+
+  
+    if (this->dim == 2)
+    {
+      // copy into Z0 for opt routines
+      for (unsigned int i = 0; i < N; ++i)
+      {
+        Z0[i]       = optdata->X0[i];
+        Z0[i + N]   = optdata->Y0[i];
+      }
+    }
+    optF ( 2*optdata->N, Z0, nullptr, optdata );
+  }
+  
+  
+  inline void runXW ()
+  {
+    double minF = optF(2*optdata->N, optdata->Z0, nullptr, optdata); 
+    if (minF > tolc)
+    {
+      std::cout <<"\n BEGIN NLOPT (X,W) \n";
+      std::cout << "Initial value of objective : " << minF << std::endl; 
+      unsigned int N = optdata->N; 
+      nlopt_opt opt, opt_i;
+      double *lb, *ub, *tolieq;
+      if (this->dim == 2)
+      {
+        // x, y > 0
+        lb = (double*) calloc(2*N, sizeof(double)); 
+        // x, y < 1
+        ub = (double*) calloc(2*N, sizeof(double));
+        tolieq = (double*) calloc(N, sizeof(double));
+        for (unsigned int i = 0; i < 2*N; ++i) 
+        { 
+          ub[i] = 1.0 - 1e-8;
+          lb[i] = 1e-8; 
+        }
+        for (unsigned int i = 0; i < N; ++i) { tolieq[i] = tolc; }
+        opt = nlopt_create(NLOPT_AUGLAG, 2*N);
+        opt_i = nlopt_create(alg, 2*N);
+        nlopt_set_local_optimizer(opt, opt_i);
+        nlopt_set_lower_bounds(opt, lb);
+        nlopt_set_upper_bounds(opt, ub);
+        nlopt_add_inequality_mconstraint(opt, N, optieqC, optdata, tolieq);
+        nlopt_set_min_objective(opt, optF, optdata);
+
+      }
+      else
+      {
+        std::cerr << "Only dim=2 is supported. Exiting..\n";
+        exit(1);
+      }
+
+      nlopt_set_xtol_rel(opt, tol);
+      nlopt_set_ftol_rel(opt, tol);
+      //nlopt_set_stopval(opt, tol);
+
+      nlopt_result result = nlopt_optimize(opt, optdata->Z0, &minF);
+      if (result < 0 && result != -4) 
+      {
+        std::cerr << "NLOPT failed! \n"; 
+        std::cerr << nlopt_result_to_string(result);
+        std::cerr << "\n Exiting ..\n";
+        exit(1);
+      }
+      else
+      {
+        if (result == -4)
+        {
+          std::cout << nlopt_result_to_string(result) << std::endl;
+        }
+        std::cout << "NLOPT converged with residual " << minF << std::endl;
+      }
+      nlopt_destroy(opt);
+      nlopt_destroy(opt_i);
+      free(lb); free(ub);
+      free(tolieq);
+      optdata->minf = minF;
+    } 
+    std::cout << "Quadrature exactness in norm up to specified order : " 
+              << minF << std::endl;
+
+    double rcond[1];
+    cond(optdata->Pm->V, optdata->N, optdata->M, rcond);
+    std::cout << "Conditioning of interpolation operator on new abscissa : "
+              << 1.0 / rcond[0] << std::endl; 
+    std::cout <<"END NLOPT \n\n";
+  }
+  
+};
+
+
+
+#endif
---
+++ src/ngjquad_redobj.cpp
@@ -0,0 +1,283 @@
+#include<ngjquad_redobj.hh>
+#include<matplotlibcpp.h>
+#include<map>
+#include<vector>
+namespace plt = matplotlibcpp;
+void plot_tri(double* tri, std::string col);
+
+
+
+/*
+
+  Usage: ./ngjquad algtype, n m tol tolc use_newton use_wolfe alph
+    -  algtype (int) - 0-2 
+        (0 - NELDERMEAD, 1 - COBYLA, 2 - SBPLX)
+    -  n,m (int) > 0
+    -  tol,tolc (double) > 0
+    -  use_newton, use_wolfe (int) - 0,1
+    -  alph (double) > 0
+  
+   n is max homogeneous poly degree in source basis 
+   m is max homogeneous poly degree in target basis
+
+   N = (n+1) choose (n-1) is number of nodes in quad rule
+   and also num polys up to total degree n in source basis
+   
+   m is max homogeneous poly degree attempted to integrate
+   exactly by quad rule of size N
+   
+   There are M polys up to total degree m in target basis 
+   we seek quad rules of size N < M 
+
+   This routine calls LAPACK complex eigenvalue routine
+   ZGEEV to compute initial nodes for optimization
+ 
+*/
+
+
+inline void set_args  ( int argc, char* argv[], 
+                        nlopt_algorithm& alg, int& dim, int& n, 
+                        int& m, double& tol, double& tolc, 
+                        bool& use_newton, bool& use_wolfe, 
+                        double& alph, unsigned int& nthreads,
+                        std::string& dir) 
+{
+  std::cout << "\nSEARCH FOR NEAR OPTIMAL GAUSSIAN QUADRATURE\n";
+  switch(argc)
+  {
+    case 11:
+    {
+      nthreads = std::stoi(argv[10]);
+      unsigned int algtype = std::stoi(argv[1]);
+      switch(algtype)
+      {
+        case 0:
+        { 
+          alg = NLOPT_LD_MMA;
+          std::cout << "ALGORITHM: SLSQP\n";
+          break;
+        }
+        case 1:
+        {
+          alg = NLOPT_LN_COBYLA;
+          std::cout << "ALGORITHM: COBYLA\n";
+          break;
+        }
+        case 2:
+        {  
+          alg = NLOPT_LN_SBPLX;
+          std::cout << "ALGORITHM: SBPLX";
+          break;
+        }
+        default:
+        {
+          std::cerr << "Algtype not supported" << std::endl;
+          break;
+        }
+      }
+      dim = std::stoi(argv[2]);
+      n = std::stoi(argv[3]);
+      m = std::stoi(argv[4]);
+      tol = std::stod(argv[5]); 
+      tolc = std::stod(argv[6]);
+      use_newton = (bool) std::stoi(argv[7]); 
+      use_wolfe = (bool) std::stoi(argv[8]);
+      alph = std::stod(argv[9]);
+      if (dim == 3 && dir == "") { dir = "../testing/testdata/"; }
+      break;
+    }
+    case 1:
+    {
+      nthreads = 1;
+      alg = NLOPT_LD_SLSQP;
+      std::cout << "ALGORITHM: SLSQP\n";
+      dim = 2; n = 4; m = 6;
+      tol = 1e-13;
+      tolc = 1e-13;
+      use_wolfe = false;
+      use_newton = false;
+      alph = 0;
+      break;
+    }
+    default:
+    {
+      std::cerr << 
+        "Usage: ./ngjquad_opt algtype dim n m tol tolc use_newton use_wolfe alph\n";
+      std::cerr << "  algtype (int) - 0-2\n";
+      std::cerr << "  dim (2,3)\n";
+      std::cerr << "  n,m (int) > 0\n";
+      std::cerr << "  tol,tolc (double) > 0\n";
+      std::cerr << "  use_newton, use_wolfe (int) - 0,1\n";
+      std::cerr << "  alph (double) > 0\n";
+      std::cerr << "  nthreads (int) > 0\n";
+      exit(1);
+    }
+  }
+  std::cout << "\nPARAMETERS:" << std::endl;
+  std::cout << std::setw(15) << "n          = " << n << std::endl;
+  std::cout << std::setw(15) << "m          = " << m << std::endl;
+  std::cout << std::setw(15) << "tol        = " << tol << std::endl;
+  std::cout << std::setw(15) << "tolc       = " << tolc << std::endl;
+  std::cout << std::setw(15) << "use_newton = " << 
+                                (use_newton ? "true\n" : "false\n");
+  std::cout << std::setw(15) << "use_wolfe  = " << 
+                                (use_newton && use_wolfe ? "true\n" : "false\n");
+  std::cout << std::setw(15) << "alpha      = " << alph << std::endl;
+  std::cout << std::setw(15) << "nthreads   = " << nthreads << std::endl;
+
+
+}
+
+inline void writeQuad(ngjQuad* gjquad, unsigned int dim)
+{
+  unsigned int N = gjquad->optdata->N;
+  if (dim == 1)
+  {
+    std::cout << "Generate the 1D quad yourself you bum!" << std::endl;
+  }
+  if (dim == 2)
+  {
+    std::stringstream ss;
+    ss  << "_N" << gjquad->optdata->N << "_n" << gjquad->optdata->n-1 
+        << "_M" << gjquad->optdata->M << "_m" << gjquad->optdata->m-1;
+    std::stringstream ssx, ssy, ssw;
+    ssx << "xtet" << ss.str() << ".txt";
+    ssy << "ytet" << ss.str() << ".txt";
+    ssw << "wtet" << ss.str() << ".txt";
+    std::ofstream xfile(ssx.str());
+    std::ofstream yfile(ssy.str());
+    std::ofstream wfile(ssw.str());
+
+    for (unsigned int i = 0; i < gjquad->optdata->N; ++i)
+    {
+      xfile << gjquad->optdata->Z0[i] << std::endl; 
+      yfile << gjquad->optdata->Z0[i+N] << std::endl; 
+      wfile << gjquad->optdata->Z0[i+2*N] << std::endl; 
+    }
+    xfile.close();
+    yfile.close();
+    wfile.close();
+  }
+  else if (dim == 3)
+  {
+    std::stringstream ss;
+    ss  << "_N" << gjquad->optdata->N << "_n" << gjquad->optdata->n-1 
+        << "_M" << gjquad->optdata->N << "_m" << gjquad->optdata->m-1;
+    std::stringstream ssx, ssy, ssz, ssw;
+    ssx << "xtet" << ss.str() << ".txt";
+    ssy << "ytet" << ss.str() << ".txt";
+    ssz << "ztet" << ss.str() << ".txt";
+    ssw << "wtet" << ss.str() << ".txt";
+    std::ofstream xfile(ssx.str());
+    std::ofstream yfile(ssy.str());
+    std::ofstream zfile(ssz.str());
+    std::ofstream wfile(ssw.str());
+    for (unsigned int i = 0; i < gjquad->optdata->N; ++i)
+    {
+      xfile << gjquad->optdata->Z0[i] << std::endl; 
+      yfile << gjquad->optdata->Z0[i+N] << std::endl; 
+      zfile << gjquad->optdata->Z0[i+2*N] << std::endl; 
+      wfile << gjquad->optdata->Z0[i+3*N] << std::endl; 
+    }
+    xfile.close();
+    yfile.close();
+    zfile.close();
+    wfile.close();
+  }
+}
+
+
+
+
+
+int main(int argc, char* argv[])
+{
+
+  unsigned int nthreads;
+
+  // parse command line args
+  nlopt_algorithm alg; 
+  int n, m; 
+  double tol, tolc, alph;
+  std::string dir;
+  int dim;
+  bool use_newton, use_wolfe; 
+  set_args( argc, argv, alg, dim, n, m, tol, tolc, 
+            use_newton, use_wolfe, alph, nthreads, dir);
+  // initialize opt routines
+  double a, b, c, d, kap; a = b = c = d = 0.5; kap = abs(a+b+c);
+  double wabc = tgamma(kap+1.5) / ( tgamma(a+0.5) * tgamma(b+0.5) * tgamma(c+0.5) );
+  double wabcd = 6;
+  ngjQuad* gjquad;
+  if (dim == 2)
+  {
+    gjquad = new ngjQuad  ( n, m, a, b, c, tol, tolc, 
+                            alph, use_newton, use_wolfe, alg,
+                            nthreads  );
+  }
+  gjquad->init();
+  double* Z0 = gjquad->optdata->Z0;
+  double* W0 = gjquad->optdata->W0; 
+  unsigned int N = gjquad->optdata->N;
+  double sumw = 0;
+  for (unsigned int i = 0; i < N; ++i) { sumw += W0[i]; } 
+  std::cout << "(initial) Sum of weights : " << sumw << std::endl;
+  gjquad->runXW();
+  sumw = 0;
+  for (unsigned int i = 0; i < N; ++i) { sumw += W0[i]; } 
+  std::cout << "(final ) Sum of weights : " << sumw << std::endl;
+  writeQuad(gjquad, dim);
+
+  if (dim == 2)
+  { 
+    // integrate test function on tri
+    double* Ftest = (double*) calloc(N, sizeof(double));
+    for (unsigned int i = 0; i < N; ++i) 
+    { 
+      Ftest[i] = std::sin( pow(Z0[i], 2) + pow(Z0[i+N], 2) ); 
+    }
+    double Ival = cblas_ddot(N, W0, 1, Ftest, 1) / wabc;
+    free(Ftest);
+
+    printf("Integral : %5.16f \n", Ival);
+    // plot resulting quadrature nodes
+    std::vector<double> X(Z0, Z0+N); std::vector<double> Y(Z0+N, Z0+2*N);
+    double tri[6] = {0, 1, 0, 0, 0, 1}; 
+    plot_tri(tri, "k-");
+    plt::plot(X, Y, "ro"); 
+    plt::show(); 
+  }
+  delete gjquad;
+  return 0;
+}
+
+void plot_tri(double* tri, std::string col)
+{
+  std::vector<double> x1, y1, x2, y2, x3, y3;
+  x1 = {tri[0], tri[1]}; y1 = {tri[3], tri[4]};
+  x2 = {tri[1], tri[2]}; y2 = {tri[4], tri[5]};
+  x3 = {tri[2], tri[0]}; y3 = {tri[5], tri[3]};
+  plt::plot(x1, y1, col);
+  plt::plot(x2, y2, col);
+  plt::plot(x3, y3, col);
+}
+
+void plot_tet()
+{
+  std::map<std::string, std::string> kwargs;
+  kwargs["marker"] = "o";
+  kwargs["linestyle"] = "-";
+  kwargs["linewidth"] = "1";
+  kwargs["markersize"] = "12";
+  std::vector<double> x, y, z;
+  x = {0, 1, 0, 0, 1};
+  y = {0, 0, 1, 0, 0};
+  z = {0, 0, 0, 1, 0};
+  plt::plot3(x, y, z);//, kwargs);
+  plt::xlim(0.0,1.0);
+  plt::ylim(0.0,1.0);
+  plt::show();
+}
+
+
+
