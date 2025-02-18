7841ae8922dc00175ea9e822dcc90880a1582276
Sachin Natesh <sachin.natesh@gmail.com>
2025-02-17 22:30:08 -0700

    add hess check to inf-dim newton stepper in dolfin/fenics

--- python/eikonal_infnewt.py
+++ python/eikonal_infnewt.py
@@ -54,31 +54,62 @@ F = (sqrt(inner(grad(u),grad(u)))*v - (1./f)*v + xi*inner(grad(u),grad(v)))*dx
 
 
 # Compute the second variation (Hessian H = d²E/du²)
-H = derivative(F, u, w)
-#H = (inner(grad(u),grad(w))*v/(sqrt(inner(grad(u),grad(u)))))*dx + xi*inner(grad(w),grad(v))*dx
+#H = derivative(F, u, w)
+H = (inner(grad(u),grad(w))*v/(sqrt(inner(grad(u),grad(u)))))*dx + xi*inner(grad(w),grad(v))*dx
 
 # Assemble the system
 #u_0 = Expression('x[0]*x[1]*(1-x[0]-x[1])',degree=1)
 #u.assign(interpolate(u_0, V))
 
+u_0 = Function(V);
 F1 = inner(grad(w), grad(v))*dx - (1./f)*v*dx
 a1, L1 = lhs(F1), rhs(F1)
-solve(a1==L1, u, bc)
+solve(a1==L1, u_0, bc)
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
-ax.scatter(X,Y,u.vector().vec())
+ax.scatter(X,Y,u_0.vector().vec())
+
+u_00 = interpolate(Expression("x[0]*(x[0]-1)*x[1]*(x[1]-1)",degree=1), V)
+u.assign(u_00)
+grad0 = assemble(F)
+H_0 = assemble(H)
+n_eps = 32
+eps = 1e-1*np.power(2., -np.arange(n_eps))
+direction = Function(V)
+direction.vector().set_local(np.random.randn(V.dim()))
+err_H = np.zeros(n_eps)
+for i in range(n_eps):
+    u.assign(u_00)
+    u.vector().axpy(eps[i], direction.vector())
+    grad_plus = assemble(F)
+    diff_grad = (grad_plus - grad0)
+    diff_grad *= 1/eps[i]
+    H_0dir = H_0 * direction.vector()
+    err_H[i] = (diff_grad - H_0dir).norm("l2")
+
+plt.figure()
+plt.loglog(eps, err_H, "-ob")
+plt.loglog(eps, (.5*err_H[0]/eps[0])*eps, "-.k")
+plt.title("Finite difference check of the second variation (Hessian)")
+plt.xlabel("eps")
+plt.ylabel("Error Hessian")
+plt.legend(['Error Hessian', 'First Order'], loc="upper left")
+plt.show()
+
+u.assign(u_0);
 
 rtol = 1e-10
 max_iter = 100
 
 pi0 = assemble(E)
 g0 = assemble(F)
+print(g0.norm("l2"))
 tol = g0.norm("l2")*rtol
 du = Function(V)
 
 lin_it = 0
 print("{0:3} {1:3} {2:15} {3:15} {4:15}".format(
-      "It", "cg_it", "Energy", "(g,du)", "||g||l2"))
+      "It", "lin_it", "Energy", "(g,du)", "||g||l2"))
 
 for i in range(max_iter):
   [J, b] = assemble_system(H, F, bc)
