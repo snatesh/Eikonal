03dd25caa50ec883aebaa4232876f111c04330ba
Sachin Natesh <sachin.natesh@gmail.com>
2025-06-19 10:15:07 -0600

    update eik fem newton step code

--- matlab/eikonal_fem_unweighted_tris.m
+++ matlab/eikonal_fem_unweighted_tris.m
@@ -12,7 +12,7 @@ W = importdata("../bin/wtri_N496_n30_M1378_m51.txt");
 %DT = delaunayTriangulation([0 0; 1 0; 0 1]);
 %DT = delaunayTriangulation([0 0; 1 0; 1 1]);
 %DT = delaunayTriangulation([0 0; 1 0; 1 1; 0 1]);
-DT = delaunay_unit_square(5);
+DT = delaunay_unit_square(2);
 meshT = DT.ConnectivityList;
 meshP = DT.Points';
 
@@ -45,9 +45,8 @@ for j = 1:size(intEdges,1)
 end
 
 % there are M total polys
-m = 20; n = m+1; M = n*(n+1)/2;
+m = 10; n = m+1; M = n*(n+1)/2;
 speed = 1;
-xi = 0.01;
 % manufactured sol and corresponding dirchlet data/rhs
 udirch = @(x,y) zeros(size(x));
 rhs = @(x,y) RHS(x,y,speed);
@@ -111,26 +110,28 @@ end
 
 
 
-% xi0 = 0.1;          % Initial penalty
-% xi_min = 1e-2;      % Do not let xi drop below this
-% xi = xi0;
-% xi_decay = 0.5;     % Reduce xi by this factor
-% xi_trigger = 1e-8;  % When residual norm drops below this, reduce xi
-% Initial nonlinear term
+xi0 = 0.1;          % Initial penalty
+xi_min = 1e-3;      % Do not let xi drop below this
+xi = xi0;
+xi_decay = 0.8;     % Reduce xi by this factor
 [N_glb, H_glb] = assemble_eik_nonlin_unweighted(cu, M, W, xi, ...
                                        V_abc, dPdP, ...
                                        meshT, meshP, K_glb);
 H_glb = H_glb';
 
+
 max_iter = 1000;
-rtol = 1e-6;
+rtol = 1e-10;
 tol = norm(N_glb + xi*K_glb*cu(1:M*nTri) - BB_id'*cu(M*nTri+1:end) - F_glb) * rtol;
+
+xi_trigger = 10*rtol;  % When residual norm drops below this, reduce xi
+
 
 fprintf("It \t (g,du) \t\t ||g|| \t\t xi\n");
 figure(3);
 for it = 1:max_iter
 
-    alph = 0.2;
+    alph = 0.25;
     % Assemble global system
     HHmat = zeros(M*nTri + nLambda);
     HHmat(1:M*nTri, 1:M*nTri) = H_glb;
@@ -157,20 +158,20 @@ for it = 1:max_iter
                                              V_abc, dPdP, ...
                                              meshT, meshP, K_glb);
     
-    %brhs = [N_glb + xi*K_glb*cu(1:M*nTri) + BB_id'*cu(M*nTri+1:end) - F_glb;
-    %        BB_id*cu(1:M*nTri)];
-    %norm_g = norm(brhs);
+    brhs = [N_glb + xi*K_glb*cu(1:M*nTri) + BB_id'*cu(M*nTri+1:end) - F_glb;
+           BB_id*cu(1:M*nTri)];
+    norm_g = norm(brhs);
 
     H_glb = H_glb';
 
-    % % Dynamically reduce xi if residual is small
-    % if norm_g < xi_trigger && xi > xi_min
-    %     xi = max(xi * xi_decay, xi_min);
-    %     [N_glb, H_glb] = assemble_eik_nonlin_unweighted(cu, M, W, xi, ...
-    %                                                     V_abc, dPdP, ...
-    %                                                     meshT, meshP, K_glb);
-    %     H_glb = H_glb';
-    % end
+    % Dynamically reduce xi if residual is small
+    if norm_g < xi_trigger && xi > xi_min
+        xi = max(xi * xi_decay, xi_min);
+        [N_glb, H_glb] = assemble_eik_nonlin_unweighted(cu, M, W, xi, ...
+                                                        V_abc, dPdP, ...
+                                                        meshT, meshP, K_glb);
+        H_glb = H_glb';
+    end
 
     fprintf("%d \t %.4e \t %.4e \t %.3e\n", it, -brhs'*du, norm_g, xi);
 
@@ -182,18 +183,19 @@ for it = 1:max_iter
         XYe = (J * [R,S]' + Pts_Ti(:,1))';
         X = XYe(:,1);
         Y = XYe(:,2);
-        T = delaunay(X,Y);
+        %T = delaunay(X,Y);
         cu_abc = cu((iTri-1)*M+1:M*iTri);
         u = V_abc*cu_abc;
-        trisurf(T,X,Y,u); hold on;
-        %scatter3(X,Y,u,'.'); drawnow; hold on; 
+        %trisurf(T,X,Y,u); hold on;
+        scatter3(X,Y,u,'.'); hold on; 
     end
     drawnow;
     hold off;
 
 
 end
-
+[XX,YY] = meshgrid(linspace(0,1,20));
+scatter3(XX(:),YY(:),distanceToUnitSquareBoundary(XX(:),YY(:)));
 %%
 figure(3);
 for iTri = 1:nTri
@@ -211,7 +213,7 @@ for iTri = 1:nTri
      trisurf(T,X,Y,u); hold on;
 end
 
-%%
+
 [XX,YY] = meshgrid(linspace(0,1,10));
 scatter3(XX(:),YY(:),distanceToUnitSquareBoundary(XX(:),YY(:)));
 %%
